fdff78f0162f3aa8cae1b8c706dd037673f17ac5
CosetteR <ys708@georgetown.edu>
2022-02-15 20:13:25 -0500

    adding design to second page

--- app/src/main/java/com/example/stopwatch/BestRecords.kt
+++ app/src/main/java/com/example/stopwatch/BestRecords.kt
@@ -2,10 +2,14 @@ package com.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import android.widget.TextView
 
 class BestRecords : AppCompatActivity() {
+    lateinit var records : ListView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_best_records)
-    }
-}
+        records = findViewById(R.id.records)
+    }//onCreate
+}//BestRecords
