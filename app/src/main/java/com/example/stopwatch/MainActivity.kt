3b36dca86de0433cb88ed5f17e0c0ff26e08b93a
dyao0210 <dyao0210@users.noreply.github.com>
2022-02-15 21:05:53 -0500

    tilted

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -1,11 +1,99 @@
 package com.example.stopwatch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.IntentFilter
+import android.text.format.Time
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.example.stopwatch.databinding.ActivityMainBinding
+import kotlin.math.roundToInt
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var timerStarted = false
+    private lateinit var serviceIntent: Intent
+    private var time = 0.0
+
+    lateinit var save: Button
+    lateinit var timeView: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startStop.setOnClickListener { startStopTimer() }
+        binding.reset.setOnClickListener { resetTimer() }
+
+
+        //startStop = findViewById(R.id.startStop)
+        save = findViewById(R.id.save)
+        //reset = findViewById(R.id.reset)
+        //timeView = findViewById(R.id.timeView)
+
+        serviceIntent = Intent(applicationContext, TimerService::class.java)
+        registerReceiver(updateTime, IntentFilter(TimerService.TIMER_UPDATED))
     }//onCreate
+
+    fun save(view: View) {
+        Toast.makeText(applicationContext, time.toString(), Toast.LENGTH_SHORT).show()
+    }
+
+    private fun resetTimer() {
+        stopTimer()
+        time = 0.0
+        binding.timeView.text = getTimeStringFromDouble(time)
+    }
+
+    private fun startStopTimer() {
+        if (timerStarted)
+            stopTimer()
+        else
+            startTimer()
+    }
+
+    private fun startTimer() {
+        serviceIntent.putExtra(TimerService.TIME_EXTRA, time)
+        startService(serviceIntent)
+        binding.startStop.text = "Stop"
+        timerStarted = true
+    }
+
+    private fun stopTimer() {
+        stopService(serviceIntent)
+        binding.startStop.text = "Start"
+        timerStarted = false
+    }
+
+    private val updateTime: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            time = intent.getDoubleExtra(TimerService.TIME_EXTRA, 0.0)
+            binding.timeView.text = getTimeStringFromDouble(time)
+        }
+    }
+
+    private fun getTimeStringFromDouble(time: Double): String {
+        val resultInt = time.roundToInt()
+        val hours = resultInt % 86400 / 3600
+        val minutes = resultInt % 86400 % 3600 / 60
+        val seconds = resultInt % 86400 % 3600 % 60
+
+        return makeTimeString(hours, minutes, seconds)
+    }
+
+    private fun makeTimeString(hour: Int, min: Int, sec: Int): String =
+        String.format("%02d:%02d:%02d", hour, min, sec)
 }//MainActivity
