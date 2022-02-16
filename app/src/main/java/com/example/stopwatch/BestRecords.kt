b8d2d5989487cf96cb97e59f0f5e2b8475f67311
CosetteR <ys708@georgetown.edu>
2022-02-15 20:58:29 -0500

    functional menu

--- app/src/main/java/com/example/stopwatch/BestRecords.kt
+++ app/src/main/java/com/example/stopwatch/BestRecords.kt
@@ -1,7 +1,11 @@
 package com.example.stopwatch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.ListView
 import android.widget.TextView
 
@@ -12,4 +16,22 @@ class BestRecords : AppCompatActivity() {
         setContentView(R.layout.activity_best_records)
         records = findViewById(R.id.records)
     }//onCreate
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater : MenuInflater = menuInflater
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }//onCreateOptionsMenu
+
+    //https://stackoverflow.com/questions/53582783/go-to-another-activity-on-menu-item-selection-in-kotlin
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.stopwatch -> {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }//onOptionsItemSelected
 }//BestRecords
--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.stopwatch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }//onCreate
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater : MenuInflater = menuInflater
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }//onCreateOptionsMenu
+
+    //https://stackoverflow.com/questions/53582783/go-to-another-activity-on-menu-item-selection-in-kotlin
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.bestRecords -> {
+                val intent = Intent(this, BestRecords::class.java)
+                startActivity(intent)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }//onOptionsItemSelected
 }//MainActivity
